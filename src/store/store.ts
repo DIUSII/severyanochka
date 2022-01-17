4ae600bdb143f9364573f495b41d1f02242ebb99
Продиус Никита <nikster.nikster@gmail.com>
2022-01-18 01:15:04 +0700

    add path

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import counterSlice from 'slice/counter/counterSlice';
+
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    counter: counterSlice
+  }
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
